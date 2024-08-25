7ed1e8051df7bbbb71b79550078b958feb0372bc
fukashin <55280398+fukashin@users.noreply.github.com>
2024-08-25 19:01:52 +0900

    add データベース接続処理をmainから分離

--- main.js
+++ main.js
@@ -2,36 +2,14 @@
 const { app } = require('electron');
 // メインウィンドウを作成する関数をインポート
 const createWindow = require('./windows/mainWindow');
-// アクティブウィンドウ監視サービスをインポート
-const startActiveWindowMonitoring = require('./services/activeWindowService');
-// データベース接続の設定をインポート
-const sequelize = require('./db');
+//データベースの接続と初期をインポート
+const initializeApp = require('./services/initializeapp');
 
-// アプリケーションの初期化を行う非同期関数
-async function initializeApp() {
-  try {
-    // データベース接続の初期化メッセージ
-    console.log('Initializing database connection...');
-    // データベースに接続
-    await sequelize.authenticate();
-    // 接続成功のメッセージ
-    console.log('Connection has been established successfully.');
-    // データベースの同期を実行
-    await sequelize.sync();
-    // 同期成功のメッセージ
-    console.log('Database synchronized successfully.');
-    // メインウィンドウを作成
-    createWindow();
-    // アクティブウィンドウの監視を開始
-    startActiveWindowMonitoring();
-  } catch (error) {
-    // データベース接続に失敗した場合のエラーメッセージ
-    console.error('Unable to connect to the database:', error);
-  }
-}
 
 // アプリケーションが準備完了したときに初期化関数を呼び出す
+// 'ready'はアプリの準備ができたときに呼ばれる
 app.on('ready', initializeApp);
+app.on('ready',createWindow);
 
 // すべてのウィンドウが閉じられたときの処理
 app.on('window-all-closed', function () {
